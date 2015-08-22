cfd90c469b5da16288b2d56546a511aaeb789ce0
Paul Logston <code@logston.me>
2015-08-22 00:39:18 -0400

    Add app role to Ansible provision.yml

---
+++ ansible/env.sh
@@ -0,0 +1,9 @@
+
+export IPY_IO_DOMAIN_NAME=ipy.vg
+
+export IPY_IO_APP_USER=vagrant
+export IPY_IO_APP_GROUP=vagrant
+export IPY_IO_APP_REPO=git://github.com/logston/ipy.io.git
+
+export IPY_IO_DOCKER_IMAGE=logston/notebook
+
--- ansible/set_env.sh
+++
@@ -1,5 +0,0 @@
-
-export IPY_IO_DOMAIN_NAME=ipy.vg
-export IPY_IO_APP_USER=vagrant
-export IPY_IO_APP_GROUP=vagrant
-
