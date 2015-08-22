284af16d7e04750aa3903658d46808bdac0a5200
Paul Logston <code@logston.me>
2015-08-22 00:50:31 -0400

    Make deploy directory configurable

--- ansible/env.sh
+++ ansible/env.sh
@@ -5,5 +5,7 @@ export IPY_IO_APP_USER=vagrant
 export IPY_IO_APP_GROUP=vagrant
 export IPY_IO_APP_REPO=git://github.com/logston/ipy.io.git
 
+export IPY_IO_DEPLOY_DIRECTORY=/srv/ipy.io
+
 export IPY_IO_DOCKER_IMAGE=logston/notebook
 
