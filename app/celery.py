d0cc7e648939abe2813312b73c51f40e2e04c62b
Paul Logston <code@logston.me>
2015-08-28 02:53:52 -0400

    Start docker container in celery task

--- app/celery.py
+++ app/celery.py
@@ -26,22 +26,18 @@ def start_container(group_id, user_id):
     :return: url of container
     :rtype: str
     """
-    client = docker.Client(base_url=app.config['DOCKER_HOST'], version='1.12')
+    client = docker.Client(base_url=app.config['DOCKER_HOST'])
     create_container_params = {
         'image': 'logston/notebook:0.1.3',
         'command': 'sh -c "jupyter notebook --notebook-dir=/srv"',
         'ports': [8888],   # Open port for business
-        #'host_config': docker.utils.create_host_config(
-        #	port_bindings={8888: 49827}
-        #),
+        'host_config': docker.utils.create_host_config(
+        	port_bindings={8888: 49827}
+        ),
     } 
     container = client.create_container(**create_container_params)
     container_id = container.get('Id')
-    print(container_id)
-    response = client.start(container=container_id, port_bindings={8888: 49827})
-    print(client.containers())
-    ports = client.port(container_id, 8888)
-    host_port = ports[0]['HostPort']
+    response = client.start(container=container_id)
    
-    return 'http://ipy.vg:{}'.format(host_port)
+    return 'http://ipy.vg:{}'.format(49827)
 
