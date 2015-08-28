9dc2bf81b08efd915b27e99d27f1562d44a39d0f
Paul Logston <code@logston.me>
2015-08-28 02:10:29 -0400

    Move to Docker Client version: 1.7.1

--- app/celery.py
+++ app/celery.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from docker import Client
+import docker
 
 from . import app
 
@@ -26,11 +26,21 @@ def start_container(group_id, user_id):
     :return: url of container
     :rtype: str
     """
-    client = Client(base_url='unix://var/run/docker.sock')
-    container = client.create_container(image='logston/notebook:0.1.3', 
-                                        command='sh -c "jupyter notebook --notebook-dir=/srv"')
-    client.start(container=container.get('Id'))
-    ports = client.port(container.get('Id'), 8888)
+    client = docker.Client(base_url=app.config['DOCKER_HOST'], version='1.12')
+    create_container_params = {
+        'image': 'logston/notebook:0.1.3',
+        'command': 'sh -c "jupyter notebook --notebook-dir=/srv"',
+        'ports': [8888],   # Open port for business
+        #'host_config': docker.utils.create_host_config(
+        #	port_bindings={8888: 49827}
+        #),
+    } 
+    container = client.create_container(**create_container_params)
+    container_id = container.get('Id')
+    print(container_id)
+    response = client.start(container=container_id, port_bindings={8888: 49827})
+    print(client.containers())
+    ports = client.port(container_id, 8888)
     host_port = ports[0]['HostPort']
    
     return 'http://ipy.vg:{}'.format(host_port)
