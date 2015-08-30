8c81d849733e84d704598b56911c616573fbff46
Paul Logston <code@logston.me>
2015-08-30 00:27:13 -0400

    Add SQLAlchemy

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-# config
-CELERY_BROKER_URL='redis://localhost:6379',
-CELERY_RESULT_BACKEND='redis://localhost:6379'
-DOCKER_HOST='unix:///var/run/docker.sock'
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 SCHEME = 'http'
 DOMAIN = 'ipy.vg'
+
+CELERY_BROKER_URL='redis://localhost:6379',
+CELERY_RESULT_BACKEND='redis://localhost:6379'
+
+DOCKER_HOST='unix:///var/run/docker.sock'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
